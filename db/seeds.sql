0751fc412d1dad34b4324cb91769275856987abd
Steve Black <SteveBlackecmpe@gmail.com>
2021-03-08 23:26:01 -0500

    Tons of updates, starter code complete, basic functions complete, Burger state not working

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burger_db;
+INSERT INTO burgers (name) VALUES ('Big Bertha');
+INSERT INTO burgers (name) VALUES ('Chaos Cannon');
+INSERT INTO burgers (name, consumed) VALUES ('Little Limby', true);
+INSERT INTO burgers (name, consumed) VALUES ('Sour Bambi', true);
+INSERT INTO burgers (name, consumed) VALUES ('Smoker Kraft', true);
+INSERT INTO burgers (name) VALUES ('DaisyDUKE');
