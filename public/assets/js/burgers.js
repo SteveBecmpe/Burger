afff90e37fdfc11c22ce4b6740a2b8aa293f7f91
Steve Black <SteveBlackecmpe@gmail.com>
2021-03-08 23:50:04 -0500

    add burger and delete burger working

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     console.info('DOM loaded');
   }
   // UPDATE
-  const changeBurgerBtns = document.querySelectorAll('.change-consume');
+  const changeBurgerBtns = document.querySelectorAll('.change-consumed');
 
   // Set up the event listener for the create button
   if (changeBurgerBtns) {
